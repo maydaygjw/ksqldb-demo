53d6eabea1674f6a90ebbb6828ebde67466f95e5
maydaygjw <maydaygjw@gmail.com>
2023-01-19 17:27:18 +0800

    init

---
+++ position_creation.sql
@@ -0,0 +1,92 @@
+CREATE TABLE t_position (
+  positionId VARCHAR PRIMARY KEY,
+  positionType VARCHAR,
+  updatedDate BIGINT,
+  accountId VARCHAR,
+  securityCode VARCHAR,
+  amount BIGINT,
+  price DECIMAL(10, 4),
+  active BOOLEAN
+) WITH (
+  KAFKA_TOPIC = 'topic_position',
+  VALUE_FORMAT = 'JSON',
+  TIMESTAMP = 'updatedDate'
+);
+
+CREATE STREAM s_position (
+  positionId VARCHAR KEY,
+  positionType VARCHAR,
+  updatedDate BIGINT,
+  accountId VARCHAR,
+  securityCode VARCHAR,
+  amount BIGINT,
+  price DECIMAL(10, 4),
+  active BOOLEAN
+) WITH (
+  KAFKA_TOPIC = 'topic_position',
+  VALUE_FORMAT = 'JSON',
+  TIMESTAMP = 'updatedDate'
+);
+
+CREATE TABLE t_account (
+  accountId VARCHAR PRIMARY KEY,
+  accountName VARCHAR,
+  updatedDate BIGINT
+) WITH (
+  KAFKA_TOPIC = 'topic_account',
+  VALUE_FORMAT = 'JSON',
+  TIMESTAMP = 'updatedDate'
+);
+
+CREATE STREAM s_account (
+  accountId VARCHAR KEY,
+  accountName VARCHAR,
+  updatedDate BIGINT
+) WITH (
+  KAFKA_TOPIC = 'topic_account',
+  VALUE_FORMAT = 'JSON',
+  TIMESTAMP = 'updatedDate'
+);
+
+create table t_position_aggr_9501_rule01 with(kafka_topic = 'topic_rule_execution_result') as
+select
+  cast(rowtime as STRING) as checkId,
+  'rule01' as ruleId,
+  ARRAY_EXCEPT(collect_list(positionId), ARRAY ['p00']) as positionId,
+  ARRAY_LENGTH(
+    ARRAY_EXCEPT(collect_list(positionType), ARRAY ['COMMIT'])
+  ) as violationCount
+from
+  t_position
+where
+  accountId = '9501'
+  and (
+    (securityCode = '600000SS') -- 禁投600000SS
+    or positionType = 'COMMIT'
+  )
+  and active = true
+group by
+  cast(rowtime as STRING)
+having
+  ARRAY_CONTAINS(collect_list(positionType), 'COMMIT') = true emit changes;
+
+create table t_position_aggr_9501_rule02 
+with(kafka_topic = 'topic_rule_execution_result') as
+select
+  securityCode,
+  AS_VALUE(securityCode) as securityCode2,
+  'rule02' as ruleId,
+  ARRAY_MAX(collect_list(rowtime)) as checkId,
+  collect_list(positionType) as positionType,
+  sum(price * amount) as upperValue,
+  1000000 as buttomValue
+from
+  t_position
+where
+  accountId = '9501'
+  and ((securityCode = '600100SS' or securityCode = '600200SS') or positionType = 'COMMIT')
+  and active = true
+group by
+  securityCode 
+  having ARRAY_LENGTH(collect_list(positionType)) > 0
+  emit changes;
---
+++ position_insertion.sql
@@ -0,0 +1,7 @@
+insert into s_position (positionId,positionType,updatedDate,accountId,securityCode,amount,price, active) values('p1001', 'NORMAL', 1674400001, '9501', '600100SS', 300, 10.2, true);
+
+insert into s_position (positionId,positionType,updatedDate,accountId,securityCode,amount,price, active) values('p8002', 'TMP', 1674500000, '9501', '600300SS', 100, 10.2, true);
+
+insert into s_position (positionId,positionType,updatedDate,accountId,securityCode,amount,price) values('p12', 'NORMAL', 1674100000, '9501', '600000SS', 1500, 13.2);
+
+insert into s_position (positionId,positionType,updatedDate,accountId,securityCode,amount,price,active) values('p00', 'COMMIT', 1674500000, '9501', '', 0, 0, true);
